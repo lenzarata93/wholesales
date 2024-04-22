3485d3797f5aaf162a95d65e3c58dd05f1c9ac87
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2024-04-22 11:37:23 +0300

    insert css module for navigation Component

--- client/wholesales project/src/components/navigationComponent.jsx
+++ client/wholesales project/src/components/navigationComponent.jsx
@@ -1,16 +1,16 @@
-
+import styles from '../components/Navigation.module.css'
 
 function Navigation (){
     return(
-        <div className='navigation'>
-            <div className="logo">
+        <div className={styles.navigation}>
+            <div className={styles.logo}>
                 <img src="https://w7.pngwing.com/pngs/450/256/png-transparent-bakery-logo-thumbnail.png"></img>
             </div>
-            <div className="site-nav">
+            <div className={styles.siteNav}>
                 <ul>
-                    <li>За нас</li>
-                    <li>Продукти</li>
-                    <li>Контакти</li>
+                    <li><a href="/aboutUs">За нас</a></li>
+                    <li><a href="/products">Продукти</a></li>
+                    <li><a href="/contacts">Контакти</a></li>
                     
                 </ul>
             </div>
