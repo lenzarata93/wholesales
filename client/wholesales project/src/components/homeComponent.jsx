afcc5764592737a640c7c92afbf0a0ee5e75493f
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2024-04-22 11:44:04 +0300

    adding some context to homeComponent

--- client/wholesales project/src/components/homeComponent.jsx
+++ client/wholesales project/src/components/homeComponent.jsx
@@ -1,8 +1,11 @@
+import styles from '../components/Home.module.css';
 function Home (){
     return(
-        <div className="home">
+        <div className={styles.home}>
             <h2>HOME PAGE.</h2>
+           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit blanditiis, dolore qui earum, quisquam explicabo nesciunt perferendis tenetur, asperiores harum assumenda enim officiis ut. Voluptatum est earum quasi molestiae sequi praesentium tenetur, ipsa beatae numquam, alias deleniti accusantium. Architecto iure illum laboriosam necessitatibus adipisci minus, ex illo tempora molestias? Nulla nesciunt praesentium veritatis harum sequi perferendis dolorum odit iusto sunt? Magni tempore dolorem iste aut beatae unde repudiandae minima? Quos eaque sunt illum veniam eum totam similique debitis sequi aspernatur expedita? A aliquam unde dolores nulla tenetur provident debitis, cupiditate eum iure magni nisi minus, eius voluptates officiis quae similique!</p>
         </div>
     )
+    
 }
 export default Home;
